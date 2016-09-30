22488bf737d664c995b67ab5c18d24bae4a3bc92
Nytra <samueltscott@gmail.com>
2016-10-01 00:00:31 +0100

    The server can now handle multiple game sessions at once
    The game sessions are now multithreaded

--- server.py
+++ server.py
@@ -58,42 +58,91 @@ def listen(s):
         co = Client(c, addr)
         clients.append(co)
 
-def print_board():
-    for n in range(0, 9, 3):
-        print(board[n] + "|" + board[n+1] + "|" + board[n+2] + "  ", n+1, "|", n+2, "|", n+3, sep="")
+def game(player1, player2):
 
-def check_win():
-    global board
-    empty = " "
-    b = board
-    if b[0] == b[1] and b[0] == b[2] and b[0] != empty:
-        return b[0]
-    elif b[0] == b[3] and b[0] == b[6] and b[0] != empty:
-        return b[0]
-    elif b[0] == b[4] and b[0] == b[8] and b[0] != empty:
-        return b[0]
-    elif b[1] == b[4] and b[1] == b[7] and b[1] != empty:
-        return b[1]
-    elif b[2] == b[5] and b[2] == b[8] and b[2] != empty:
-        return b[2]
-    elif b[3] == b[4] and b[3] == b[5] and b[3] != empty:
-        return b[3]
-    elif b[6] == b[4] and b[6] == b[2] and b[6] != empty:
-        return b[6]
-    elif b[6] == b[7] and b[6] == b[8] and b[6] != empty:
-        return b[6]
+    def print_board(): # display this instance's board
+        for n in range(0, 9, 3):
+            print(board[n] + "|" + board[n + 1] + "|" + board[n + 2] + "  ", n + 1, "|", n + 2, "|", n + 3, sep="")
+
+    def check_win(): # check this instance's board for a win
+        empty = " "
+        b = board
+        winner = ""
+        if b[0] == b[1] and b[0] == b[2] and b[0] != empty:
+            winner = b[0]
+        elif b[0] == b[3] and b[0] == b[6] and b[0] != empty:
+            winner = b[0]
+        elif b[0] == b[4] and b[0] == b[8] and b[0] != empty:
+            winner = b[0]
+        elif b[1] == b[4] and b[1] == b[7] and b[1] != empty:
+            winner = b[1]
+        elif b[2] == b[5] and b[2] == b[8] and b[2] != empty:
+            winner = b[2]
+        elif b[3] == b[4] and b[3] == b[5] and b[3] != empty:
+            winner = b[3]
+        elif b[6] == b[4] and b[6] == b[2] and b[6] != empty:
+            winner = b[6]
+        elif b[6] == b[7] and b[6] == b[8] and b[6] != empty:
+            winner = b[6]
+        else:
+            winner = empty
+        return winner.strip()
+
+    def game_over():
+        player1.c.send("over||{}||{}".format(winner, ",".join(x for x in board)).encode())
+        player2.c.send("over||{}||{}".format(winner, ",".join(x for x in board)).encode())
+
+    def next_turn(this_turn):
+        if this_turn == player1:
+            this_turn = player2
+        else:
+            this_turn = player1
+        return this_turn
+
+
+    board = [" "] * 9
+    winner = None
+    this_turn = random.choice((player1, player2)) # randomly select a player to go first
+
+    this_turn.set_counter("X") # first turn always gets X
+    if this_turn == player1:
+        player2.set_counter("O")
     else:
-        return empty
+        player1.set_counter("O")
+
+    while not winner:
+        this_turn.c.send("go||{}||{}".format(this_turn.get_counter(), ",".join(x for x in board)).encode())
+        print("It is ", this_turn.get_addr(), "'s turn", sep="")
+        index = None
+        while index == None:
+            data = this_turn.get_data()
+            if not data:
+                this_turn.kill()
+            response = data.decode("utf-8")
+            if int(response) in range(9):
+                index = int(response)
+                print("Got position:", index + 1)
+                if board[index] == " ":
+                    board[index] = this_turn.get_counter()
+                    print_board()
+                    winner = check_win()
+                    this_turn = next_turn(this_turn)
+                else:
+                    print("Invalid position.")
+                    this_turn.c.send("invalid_pos||{}||{}".format(this_turn.get_counter(),
+                                                                  ",".join(x for x in
+                                                                           board)).encode())
+            else:
+                this_turn.c.send(
+                    "invalid_pos||{}||{}".format(this_turn.get_counter(), ",".join(x for x in board)).encode())
+    print(winner, "wins!")
+    game_over()
 
 def client():
     global clients
     while True:
         for client in clients:
             if client.is_alive():
-                #print("Client", client.get_addr(), "is alive")
-                #data = client.get_data()
-                #if not data:
-                    #client.kill()
                 if client.get_opponent() == None:
                     for match in clients:
                         if match.get_addr() == client.get_addr():
@@ -103,47 +152,12 @@ def client():
                                 print("Matching", match.addr, "with", client.get_addr())
                                 match.set_opponent(client.get_addr())
                                 client.set_opponent(match.get_addr())
-
-                                client.c.send("ready".encode())
+                                client.c.send("ready".encode()) # tell that clients to prepare for the game
                                 match.c.send("ready".encode())
-                                winner = None
-                                this_turn = random.choice((match, client))
-                                match.set_counter("X")
-                                client.set_counter("O")
-                                while not winner:
-                                    this_turn.my_turn = True
-                                    this_turn.c.send("go||{}||{}".format(this_turn.get_counter(), ",".join(x for x in board)).encode())
-                                    print("It is ", this_turn.get_addr(), "'s turn", sep="")
-                                    index = None
-                                    while index == None:
-                                        data = this_turn.get_data()
-                                        if not data:
-                                            this_turn.kill()
-                                        response = data.decode("utf-8")
-                                        if int(response) in range(9):
-                                            index = int(response)
-                                            print("Got position:", index+1)
-                                            if board[index] == " ":
-                                                board[index] = this_turn.get_counter()
-                                                print_board()
-                                                winner = check_win().split()
-                                                if this_turn == match:
-                                                    this_turn = client
-                                                else:
-                                                    this_turn = match
-                                            else:
-                                                print("Invalid position.")
-                                                this_turn.c.send("invalid_pos||{}||{}".format(this_turn.get_counter(),
-                                                                                              ",".join(x for x in
-                                                                                                       board)).encode())
-                                        else:
-                                            this_turn.c.send("invalid_pos||{}||{}".format(this_turn.get_counter(), ",".join(x for x in board)).encode())
-                                print(winner, "wins!")
-                                match.c.send("over||{}||{}".format(winner, ",".join(x for x in board)).encode())
-                                client.c.send("over||{}||{}".format(winner, ",".join(x for x in board)).encode())
+                                tg = threading.Thread(target=game, args=(client, match))
+                                tg.start() # start the game thread
 
 clients = []
-board = [" "] * 9
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server = socket.gethostbyname(socket.gethostname())
